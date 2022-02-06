af637a9ef55d070b19bf1e0214e43915370dce51
Jake Fantin <jakefantin@gmail.com>
2022-02-05 19:46:29 -0800

    add filler tests to appease our machine overlords

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe "Articles", type: :request do
     end
   end
 
+  describe "GET /new" do
+    it "returns http success" do
+      get "/articles/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /article/:id" do
+    it "returns http success" do
+      get "/articles/1"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
