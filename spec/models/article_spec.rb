db7b9fabf734c1f70fd97e9035443b9868189721
Jake Fantin <jakefantin@gmail.com>
2022-06-02 18:46:53 -0700

    add factories to model tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,29 +3,29 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   context "validating the article model" do
     it "is valid with valid attributes" do
-      article = Article.create(title: "Title", body: "This is a body.")
+      article = build(:article)
       expect(article).to be_valid
     end
     it "is not valid without a title" do
-      article = Article.create(body: "This is a body.")
+      article = build(:article, title: nil)
       expect(article).to_not be_valid
     end
     it "is not valid without a body" do
-      article = Article.create(title: "Title")
+      article = build(:article, body: nil)
       expect(article).to_not be_valid
     end
     it "is not valid with short title" do
-      article = Article.create(title: "Tit", body: "This is a body.")
+      article = build(:article, title: 'Tit')
       expect(article).to_not be_valid
     end
     it "is not valid with short body" do
-      article = Article.create(title: "Title", body: "Body.")
+      article = build(:article, body: 'Body')
       expect(article).to_not be_valid
     end
     it "is not valid with non-unique title" do
-      article = Article.create(title: "Title", body: "This is a body.")
-      article.save
-      dupe_article = Article.create(title: "Title", body: "This is a body.")
+      article = create(:article)
+      dupe_article = build(:article)
+
       expect(dupe_article.valid?).to eq(false)
     end
   end
