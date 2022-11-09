14969f79ee378f31f1060935b28ba8c33e2f6b02
Jake Fantin <jakefantin@gmail.com>
2022-11-09 13:41:14 -0800

    overload to_param for slug

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
   validates :title, presence: true, uniqueness: { case_sensitive: false}, length: { minimum: 5 }
   validates :body, presence: true, length: { minimum: 10 }
+
+  # change the slug to be the id followed by the title
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end
 end
